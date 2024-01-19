abd74792e4a7cb67ba18b748e6370b2ca2255efe
andluo <1455234749@qq.com>
2024-01-18 23:46:38 -0800

    return token

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,16 +1,23 @@
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingService } from '../hashing/hashing.service';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
 
 @Injectable()
 export class AuthenticationService {
     constructor(
         @InjectRepository(User) private readonly usersRepository:Repository<User>,
-        private readonly hashingService:HashingService
+        private readonly hashingService:HashingService,
+        private jwtService:JwtService,
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration:ConfigType<typeof jwtConfig>
+        
     ){}
 
     async signUp(signUpDto:SignUpDto){
@@ -35,7 +42,25 @@ export class AuthenticationService {
             if(!isEqual){
                 throw new UnauthorizedException('密码不正确.')
             }
-            return true
+            console.log(user);
+
+            console.log(this.jwtConfiguration);
+            
+            const accessToken = await this.jwtService.signAsync(
+                {
+                    sub:user.id,
+                    email:user.email
+                },
+                {
+                    audience:this.jwtConfiguration.audience,
+                    issuer:this.jwtConfiguration.issuer,
+                    secret:this.jwtConfiguration.secret,
+                    expiresIn:this.jwtConfiguration.accessTokenTtl,
+                }
+            )
+            return {
+                accessToken
+            }
         }else{
             throw new UnauthorizedException('该用户稍未注册.')
         }
