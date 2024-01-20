f36698a954c808e57d8c49944b9b9e649f71d8f5
andluo <1455234749@qq.com>
2024-01-19 20:55:18 -0800

    refresh token

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -4,6 +4,7 @@ import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 import { Auth } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type-enums';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Auth(AuthType.None)
 @Controller('authentication')
@@ -21,5 +22,11 @@ export class AuthenticationController {
     @Post('sign-in')
     signIn(@Body() signInDto:SignInDto){
         return this.authService.signIn(signInDto)
+    } 
+    
+    @HttpCode(HttpStatus.OK)
+    @Post('refresh-token')
+    refreshToken(@Body() refreshTokenDto:RefreshTokenDto){
+        return this.authService.refreshToken(refreshTokenDto)
     }    
 }
--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -9,62 +9,83 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
 import { ActiveUserData } from '../interfaces/active-user-data.interface';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Injectable()
 export class AuthenticationService {
     constructor(
-        @InjectRepository(User) private readonly usersRepository:Repository<User>,
-        private readonly hashingService:HashingService,
-        private jwtService:JwtService,
+        @InjectRepository(User) private readonly usersRepository: Repository<User>,
+        private readonly hashingService: HashingService,
+        private jwtService: JwtService,
         @Inject(jwtConfig.KEY)
-        private readonly jwtConfiguration:ConfigType<typeof jwtConfig>
-        
-    ){}
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>
 
-    async signUp(signUpDto:SignUpDto){
+    ) { }
+
+    async signUp(signUpDto: SignUpDto) {
         try {
             const user = new User()
             user.email = signUpDto.email
             user.password = await this.hashingService.hash(signUpDto.password)
             await this.usersRepository.save(user)
         } catch (error) {
-            if(error.code === '23505'){
+            if (error.code === '23505') {
                 throw new ConflictException()
             }
             throw error
         }
-        
+
     }
 
-    async signIn(signInDto:SignInDto){
-        const user = await this.usersRepository.findOneBy({email:signInDto.email})
-        if(user){
-            const isEqual = await this.hashingService.compare(signInDto.password,user.password)
-            if(!isEqual){
+    async signIn(signInDto: SignInDto) {
+        const user = await this.usersRepository.findOneBy({ email: signInDto.email })
+        if (user) {
+            const isEqual = await this.hashingService.compare(signInDto.password, user.password)
+            if (!isEqual) {
                 throw new UnauthorizedException('密码不正确.')
             }
-
-            
-            const accessToken = await this.signToken(user)
-            return {
-                accessToken
-            }
-        }else{
+            return await this.generateToken(user);
+        } else {
             throw new UnauthorizedException('该用户稍未注册.')
         }
     }
 
-    private async signToken(user: User) {
+    async generateToken(user: User) {
+        const [accessToken, refreshToken] = await Promise.all([
+            await this.signToken<Partial<ActiveUserData>>(user.id, this.jwtConfiguration.accessTokenTtl, { email: user.email }),
+            await this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+        ]);
+
+        return {
+            accessToken, refreshToken
+        };
+    }
+
+    async refreshToken(refreshTokenDto:RefreshTokenDto){
+        try {
+                        const {sub} = await this.jwtService.verifyAsync<Pick<ActiveUserData,'sub'>>(refreshTokenDto.refreshToken,{
+                audience: this.jwtConfiguration.audience,
+                issuer: this.jwtConfiguration.issuer,
+                secret: this.jwtConfiguration.secret,
+            })
+            const user = await this.usersRepository.findOneByOrFail({id:sub})
+            return this.generateToken(user)
+        } catch (error) {
+            throw new UnauthorizedException()
+        }
+    }
+
+    private async signToken<T>(userId: number, expiresIn: number, payload?: T) {
         return await this.jwtService.signAsync(
             {
-                sub: user.id,
-                email: user.email
-            } as ActiveUserData,
+                sub: userId,
+                ...payload,
+            },
             {
                 audience: this.jwtConfiguration.audience,
                 issuer: this.jwtConfiguration.issuer,
                 secret: this.jwtConfiguration.secret,
-                expiresIn: this.jwtConfiguration.accessTokenTtl,
+                expiresIn: expiresIn,
             }
         );
     }
